b6c26efdf3751dfbf0e439d329d0c7d6a567ac07
Ely Karr <elykarr@gmail.com>
2022-01-28 13:25:31 -0800

    commit tree 9d977a16e7a1dbdcb8536aa1c3389e7ef157ad34

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, sex) VALUES ("Mr. Chocolate", 1, "M");
-INSERT INTO bears (name, age, sex) VALUES ("Roudy", 5, "M");
-INSERT INTO bears (name, age, sex) VALUES ("Tabitha", 11, "F");
-INSERT INTO bears (name, age, sex) VALUES ("Sergeant Brown", 400,"M");
-INSERT INTO bears (name, age, sex) VALUES ("Melissa", 3, "F");
-INSERT INTO bears (name, age, sex) VALUES ("Grinch", 2, "M");
-INSERT INTO bears (name, age, sex) VALUES ("Wendy", 0, "F");
-INSERT INTO bears (name, age, sex) VALUES (NULL, 8, "F");
+INSERT INTO bears (name, age, sex, alive) VALUES ("Mr. Chocolate", 1, "M", TRUE);
+INSERT INTO bears (name, age, sex, alive) VALUES ("Roudy", 5, "M", FALSE);
+INSERT INTO bears (name, age, sex, alive) VALUES ("Tabitha", 11, "F", TRUE);
+INSERT INTO bears (name, age, sex, alive) VALUES ("Sergeant Brown", 400,"M", FALSE);
+INSERT INTO bears (name, age, sex, alive) VALUES ("Melissa", 3, "F", TRUE);
+INSERT INTO bears (name, age, sex, alive) VALUES ("Grinch", 2, "M", TRUE);
+INSERT INTO bears (name, age, sex, alive) VALUES ("Wendy", 0, "F", FALSE);
+INSERT INTO bears (name, age, sex, alive) VALUES (NULL, 8, "F", TRUE);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,13 +7,13 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT bears.name, bears.age, FROM bears ORDER BY age DESC;"
+  "SELECT bears.name, bears.age FROM bears WHERE alive = TRUE ORDER BY age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears ORDER BY age ASC LIMIT 1;"
+  "SELECT bears.name, bears.age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT * FROM bears ORDER BY age ASC LIMIT 1;"
+  "SELECT bears.name, bears.age FROM bears ORDER BY age ASC LIMIT 1;"
 end
