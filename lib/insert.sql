d2043661ef891c873158430e17eabe81bb03922b
Ely Karr <elykarr@gmail.com>
2022-01-28 13:03:46 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES ("Mr. Chocolate", 1, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Roudy", 5, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Tabitha", 11, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Sergeant Brown", 400,"M");
+INSERT INTO bears (name, age, sex) VALUES ("Melissa", 3, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Grinch", 2, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Wendy", 0, "F");
+INSERT INTO bears (name, age, sex) VALUES (NULL, 8, "F");
